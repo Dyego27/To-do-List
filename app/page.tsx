6016b8c241c12ee867df7c3b73c7fe9aabc71fc3
Dyego27 <dyegoluis123@gmail.com>
2025-11-03 17:00:05 -0300

    Adicionando mais outro component Separator

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,22 @@
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+import { PlusIcon } from "lucide-react";
 export default function Home() {
   return (
-    <h1>Inicio</h1>
+    <main className="w-full h-screen bg-gray-100 flex justify-center items-center">
+      <Card className="w-lg p-4">
+        <div className="flex gap-2">
+          <Input placeholder="Adicionar tarefa" />
+          <Button variant={"outline"} className="cur">
+            {" "}
+            <PlusIcon /> Cadastrar
+          </Button>
+        </div>
+
+        <Separator />
+      </Card>
+    </main>
   );
 }
